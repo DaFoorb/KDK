ace6527dffb5897a9deae405ba429adc9f1d7f1f
DaFoorb <o.cornupro@gmail.com>
2015-10-16 21:56:59 +0200

    Creation
    Creation of the GitHub

---
+++ Assets/Script/Event.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Event : MonoBehaviour {
+
+	//public Camera currentCamera ;
+	RuntimePlatform pl= Application.platform ;
+	RaycastHit2D raycast ;
+	Ray ray ;
+	Physics2D ph2d ;
+
+	// Use this for initialization
+	void Start () {
+		//raycast= new RaycastHit2D() ;
+		Debug.Log ("TU RENTRES ?") ;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// /!\ Obliger de tester sur un téléphone à cause du Input.touchCount
+			for (int n_countPush= 0 ;
+			     n_countPush < Input.touchCount ;
+			     n_countPush++) {
+
+				if (Input.GetTouch(n_countPush).phase == TouchPhase.Began) {
+					RaycastHit2D rh2d= Physics2D.Raycast(Input.GetTouch(n_countPush).position,
+					                                     -Vector2.up) ;
+					if (rh2d != null) {
+						rh2d.transform.gameObject.GetComponent<SpriteRenderer>().color.Equals(Color.red) ;
+					}
+				}
+			}
+		}
+	}
+
---
+++ Assets/Script/GenerateDot.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateDot : MonoBehaviour {
+	public Transform tr_dot ;
+	int i_rdmDots ;
+	
+	void Start () {
+		// On génère un nombre aléatoire de points pour la figure en cours
+		int i_rdmDots= Random.Range(2, 5) ;
+
+		for (int i_it= 0 ;
+		     i_it < i_rdmDots ;
+		     i_it++) {
+			Transform tmp= tr_dot ;
+			tmp.name= "Dot_"+ i_it ;
+			Instantiate(tmp, 
+			            new Vector3(Random.Range (-8, 8), 
+			            			Random.Range(-13, 13), 
+			            			0), 
+			            Quaternion.identity) ;
+			}
+		}
+
+	void Update () {
+	
+	}
+}
